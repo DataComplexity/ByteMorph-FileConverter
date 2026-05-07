cbb6412c4a5a2dbdbeba49bc72c5a2650aec1634
DataComplexity <lakshyasaini.work12345@gmail.com>
2026-05-08 01:03:10 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -73,7 +73,7 @@ app.use('/api/auth', authRoutes);
 const distPath = path.join(__dirname, '../dist');
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get('*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     if (!req.path.startsWith('/api')) {
       res.sendFile(path.join(distPath, 'index.html'));
     }
